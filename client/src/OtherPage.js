ee19544afa4c50403338e8c1a2b67c2af52fadc8
Vishal Kumar Singh <01vksingh@gmail.com>
2019-10-03 11:41:28 +0530

    first commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default () =>{
+  return(
+    <div>
+      i m somether page
+      <Link to="/">go back simon</Link>
+    </div>
+  );
+};
